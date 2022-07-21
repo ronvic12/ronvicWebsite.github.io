bbd00fdfc92f691cd217b0bb84830c69d036eb4c
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-07-21 00:26:51 -0700

    fix the margins

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,11 +2,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {Link,NavLink} from 'react-router-dom';
 import './index.scss';
 import {faHome,faUser,faCode,faTimeline} from '@fortawesome/free-solid-svg-icons';
-
+import Ronvic from '../../assets/images/ronvic.png';
 
 const Sidebar = () => (
     <div className= 'nav-bar'>
         <Link className= 'logo' to ='/'>
+            <img src ={Ronvic}></img>
+            <h2>Ronvic Cuevas</h2>
         </Link>
         <nav>
             <NavLink exact = "true" activeclassname = "active" to ="/">
