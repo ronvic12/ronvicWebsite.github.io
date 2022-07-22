d55fc394f33e831ccb9241314efe5bc2c06d7d6b
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-07-22 00:05:34 -0700

    need to finish about page tom

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,22 +5,22 @@ import {faHome,faUser,faCode,faTimeline} from '@fortawesome/free-solid-svg-icons
 import Ronvic from '../../assets/images/ronvic.png';
 
 const Sidebar = () => (
-    <div className= 'nav-bar'>
-        <Link className= 'logo' to ='/'>
-            <img src ={Ronvic}></img>
+    <div className= "nav-bar">
+        <Link className= 'logo' to ="/">
+            <img src ={Ronvic} alt ="Logo"></img>
             <h2 className= 'title'>Ronvic Cuevas</h2>
         </Link>
         <nav>
             <NavLink exact = "true" activeclassname = "active" to ="/">
                 <FontAwesomeIcon icon ={faHome} color= "#4d4d4e"/>
             </NavLink>
-            <NavLink exact = "true" activeclassname = "active" className ="about-link" to ="/about">
+            <NavLink  activeclassname = "active" className ="about-link" to ="/about">
                 <FontAwesomeIcon icon ={faUser} color= "#4d4d4e"/> 
             </NavLink>
-            <NavLink exact = "true" activeclassname = "active" className ="project-link" to ="/projects">
+            <NavLink  activeclassname = "active" className ="project-link" to ="/projects">
                 <FontAwesomeIcon icon ={faCode} color= "#4d4d4e"/> 
             </NavLink>
-            <NavLink exact = "true" activeclassname = "active" className ="exp-link"to ="/workexperience">
+            <NavLink activeclassname = "active" className ="exp-link"to ="/workexperience">
                 <FontAwesomeIcon icon ={faTimeline} color= "#4d4d4e"/> 
             </NavLink>
         </nav>
