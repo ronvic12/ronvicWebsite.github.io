847afcb0745410b595ecf30269cf6a5d6f840e02
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-07-22 13:52:49 -0700

    need to fix icons

---
+++ src/components/About/index.js
@@ -0,0 +1,10 @@
+const About = () =>{
+
+    return (
+        <div className="container about-page">
+           <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default About;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,13 @@
 import Profile from '../../assets/images/ronvic.png';
-import {Link} from 'react-router-dom';
 import './index.scss';
 import Resume from '../../assets/resume/software engineer resume 2022 (Early Career) - Google Docs.pdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLinkedin,
+    faGithub,
+    faInstagram,
+    faTwitter,
+  } from '@fortawesome/free-brands-svg-icons'
 const Home = () =>{
 
     return (
@@ -13,7 +19,19 @@ const Home = () =>{
             <h1>Software Developer</h1>
             <h2>Computer Science at UC Davis</h2>
             <a href={Resume} className ="resume-button">Download Resume</a>
-      
+            </div>
+            <div className ="icons">
+                <ul>
+                    <a
+                    target="_blank"
+                    rel ="noreferrer"
+                    href = "https://www.linkedin.com/in/ronvic-cuevas/"
+                    ></a>
+                    <FontAwesomeIcon icon = {faLinkedin} color="#4d4d4e"/>
+                    <FontAwesomeIcon icon = {faGithub} color="#4d4d4e"/>
+                    <FontAwesomeIcon icon = {faInstagram} color="#4d4d4e"/>
+                    <FontAwesomeIcon icon = {faTwitter} color="#4d4d4e"/>
+                </ul>
             </div>
         </div>
     );
