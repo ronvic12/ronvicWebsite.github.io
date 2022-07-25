29ad8374b19bfeebe2295baf117265f73b92f40c
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-07-25 10:39:29 -0700

    added files

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {Link,NavLink} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './index.scss';
 import {faHome,faUser,faCode,faTimeline} from '@fortawesome/free-solid-svg-icons';
-import Ronvic from '../../assets/images/ronvic.png';
+
 import {
     faLinkedin,
     faGithub
   } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
     <div className= "nav-bar">
-        <Link className= 'logo' to ="/">
-            <img src ={Ronvic} alt ="Logo"></img>
-            <h2 className= 'title'>Ronvic Cuevas</h2>
-        </Link>
         <nav>
             <NavLink exact = "true" activeclassname = "active" to ="/">
                 <FontAwesomeIcon icon ={faHome} color= "#4d4d4e"/>
