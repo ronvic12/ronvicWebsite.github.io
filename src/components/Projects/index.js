24caf50e7e7d0c7ac1143c12b15f8d93785d8984
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-07-25 20:56:56 -0700

    added card-group

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -12,63 +12,171 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
 import CardGroup from 'react-bootstrap/CardGroup';
-import KnightQuest from '../../assets/logo_images/knightquest.png'
-const Projects = () => {
+import KnightQuest from '../../assets/logo_images/kq.png'
+
+// function Card(props){
+//   return(
+//     <div className="card">
+//       <div className="card_body">
+//         <img src ={props.img}/>
+//         <h2 className="card_title">{props.title}</h2>
+//         <p className="card_description">{props.description}</p>
+//         <button className="card_btn">View on Github</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+function Projects(){
   return (
-  <div className ="project-container">
-{/* <div class="card bg-dark text-white">
-  <img id ="profimg"src={KnightQuest}  alt="Card-image"/>
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text">Last updated 3 mins ago</p>
-  </div>
-</div> */}
+    <div className="wrapper">
 
     <CardGroup>
-      <Card>
-        <Card.Img id = "profimg" variant="top" src={KnightQuest} class ="rounded-0"/>
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
-</div>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={KnightQuest} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+      <Button variant="primary">View on Github</Button>
+    </Card.Body>
+  </Card>
+  </CardGroup>
+  </div>
   );
 };
 
 export default Projects;
+// <CardGroup>
+    //   <Card>
+    //     <Card.Img id = "profimg" variant="top" src={KnightQuest} />
+    //     <Card.Body>
+    //       <Card.Title>Card title</Card.Title>
+    //       <Card.Text>
+    //         This is a wider card with supporting text below as a natural lead-in
+    //         to additional content. This content is a little bit longer.
+    //       </Card.Text>
+    //     </Card.Body>
+    //     <Card.Footer>
+    //       <small className="text-muted">Last updated 3 mins ago</small>
+    //     </Card.Footer>
+    //   </Card>
+    //   <Card>
+    //     <Card.Img variant="top" src="holder.js/100px160" />
+    //     <Card.Body>
+    //       <Card.Title>Card title</Card.Title>
+    //       <Card.Text>
+    //         This card has supporting text below as a natural lead-in to
+    //         additional content.{' '}
+    //       </Card.Text>
+    //     </Card.Body>
+    //     <Card.Footer>
+    //       <small className="text-muted">Last updated 3 mins ago</small>
+    //     </Card.Footer>
+    //   </Card>
+    //   <Card>
+    //     <Card.Img variant="top" src="holder.js/100px160" />
+    //     <Card.Body>
+    //       <Card.Title>Card title</Card.Title>
+    //       <Card.Text>
+    //         This is a wider card with supporting text below as a natural lead-in
+    //         to additional content. This card has even longer content than the
+    //         first to show that equal height action.
+    //       </Card.Text>
+    //     </Card.Body>
+    //     <Card.Footer>
+    //       <small className="text-muted">Last updated 3 mins ago</small>
+    //     </Card.Footer>
+    //   </Card>
+    // </CardGroup>
