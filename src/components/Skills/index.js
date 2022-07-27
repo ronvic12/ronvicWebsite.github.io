6f3fcd5ed17e74a76d6efaf87f318a3d76c862d7
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-07-26 19:32:37 -0700

    added contents in skills section

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -9,7 +9,43 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Skills = () => {
   return (
-  <div><h1>Skills page</h1></div>
+  <div className="skills-page">
+    <h1>SKILLS</h1>
+
+    <h1>Programming Languages</h1>
+    <div className="programming">
+    <ul id = "types">
+      <li>Python</li>
+      <li>C/C++</li>
+      <li>Dart</li>
+    </ul>
+    </div>
+    <h1>Front-end Tools & FrameWorks (Mobile/Web Dev)</h1>
+    <div className="programming">
+    <ul id = "types">
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>TypeScript/JavaScript</li>
+      <li>ReactJS</li>
+      <li>Flutter</li>
+      <li>Figma</li>
+      <li>Swift/SwiftUI</li>
+    </ul>
+    </div>
+    <h1>Backend tools & Databases (Mobile/Web Dev)</h1>
+    <div className="programming">
+    <ul id = "types">
+      <li>MySQL</li>
+      <li>Express.js</li>
+      <li>Next.js</li>
+      <li>Nest.js</li>
+      <li>Node.js</li>
+      <li>Firebase</li>
+      <li>RethinkDB</li>
+    </ul>
+    </div>
+    
+    </div>
   );
 };
 
