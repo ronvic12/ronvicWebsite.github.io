00d7bb4c814bd5b8dda0151539ca38453d72b9be
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2022-07-26 21:29:58 -0700

    Updates

--- src/components/WorkIcon/index.js
+++
@@ -1,9 +0,0 @@
-import { faBriefcase} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const WorkIcon = () => {
-  return (
-    <FontAwesomeIcon icon={faBriefcase} />
-  );
-};
-
-export default WorkIcon;
